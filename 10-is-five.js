179055df49328dd1cafb24776f06e0192b6f26e5
akumaluna <ldlvvna@gmail.com>
2023-03-10 15:50:25 -0700

    is-5

---
+++ 10-is-five.js
@@ -0,0 +1,15 @@
+/*
+
+Define a function isFive that will return true if a number is equal to 5 and false if it is not.
+
+*/
+
+
+let isFive = num => {
+    if (num === 5) return true;
+    return false;
+}
+
+console.log(isFive(5)); // true
+console.log(isFive(13)); // false
+console.log(isFive(25)); //false
