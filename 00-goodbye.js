4ef599bf50ee61d249bc30b1a18f059ea03280f5
akumaluna <ldlvvna@gmail.com>
2023-03-10 15:14:05 -0700

    init commit

---
+++ 00-goodbye.js
@@ -0,0 +1,12 @@
+/*
+
+Write a function goodbye(name) that takes in a string name and returns a string saying "bye" to that name. See the examples below.
+
+*/
+
+
+let goodbye = name => "Bye " + name + ".";
+
+  console.log(goodbye("Daniel")); // => "Bye Daniel."
+  console.log(goodbye("Mark")); // => "Bye Mark."
+  console.log(goodbye("Beyonce")); // => "Bye Beyonce."
---
+++ 01-plus-5.js
@@ -0,0 +1,12 @@
+/*
+
+Write a function plusFive that takes in a number as an argument and returns the sum of that number and 5.
+
+*/
+
+
+let plusFive = num => num + 5;
+
+  console.log(plusFive(10));  // => 15
+  console.log(plusFive(2));   // => 7
+  console.log(plusFive(-8));  // => -3
